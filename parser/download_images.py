fd224c570df9286a41b9951e3262da9cdcffb1b3
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-12-03 06:16:28 +0300

    Add parsering part

---
+++ parser/download_images.py
@@ -0,0 +1,34 @@
+import asyncio
+import aiohttp
+import aiofiles
+import os
+
+
+async def write_file(session, category, url, name_img):
+    async with aiofiles.open(f'../data/images/{category}/{name_img}.jpg', mode='wb') as file:
+        async with session.get(url) as response:
+            async for x in response.content.iter_chunked(1024):
+                await file.write(x)
+
+
+async def main(category, links):
+    async with aiohttp.ClientSession() as session:
+
+        tasks = []
+        for link in links:
+            name = hash(link)
+            task = asyncio.create_task(write_file(session, category, link, name))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+
+def main_process(category):
+    with open(f'../data/links/{category}.txt', 'r') as file_:
+        urls = file_.read().split('\n')
+    os.mkdir(f'../data/images/{category}')
+    asyncio.run(main(category, urls))
+
+
+if __name__ == '__main__':
+    cat = input()
+    main_process(cat)
---
+++ parser/parser_links.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+more_button = 'button2 button2_size_l button2_theme_action button2_type_link button2_view_classic more__button i-bem button2_js_inited'
+
+
+def make_category(browser_: webdriver, category: str):
+    url = f'https://yandex.ru/images/search?text={category}'
+    browser_.get(url)
+    time.sleep(1)
+
+    for i in range(700):
+        browser_.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        if i % 30 == 0:
+            time.sleep(1)
+        try:
+            browser_.find_element(
+                by=By.CLASS_NAME,
+                value='more more_direction_next i-bem more_js_inited more_last_yes'
+            ).find_element(
+                by=By.CLASS_NAME,
+                value=more_button,
+            ).click()
+        except:
+            pass
+
+    a_tags = browser_.find_elements(
+        by=By.CLASS_NAME,
+        value='serp-item__link',
+    )
+    print(f'For category {category} is found {len(a_tags)} images')
+    links = [a.find_element(By.TAG_NAME, 'img').get_attribute('src') + '\n'
+             for a in a_tags]
+    links[-1] = links[-1].strip('\n')
+    with open(f'../data/links/{category}.txt', 'w') as file:
+        file.writelines(links)
+
+
+def main(category):
+    with webdriver.Chrome() as browser:
+        browser.maximize_window()
+        make_category(browser, category)
+
+
+if __name__ == '__main__':
+    cat_name = input()
+    main(cat_name)
